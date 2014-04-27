aaf147533e814b6bbc14441a16ccecac4a8c4638
youmahil <youmahil@bluedigm.com>
2014-04-27 17:07:39 +0900

    add class

---
+++ testgit/src/main/java/com/testgit/Testgit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.testgit;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Testgit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
